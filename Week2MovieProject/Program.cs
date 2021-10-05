7660852d05f83c2acbde6dec83f819f7df7b9b0d
unknown <will.bridgeman@yahoo.co.uk>
2021-10-05 11:19:09 +0100

    update

--- Week2MovieProject/Program.cs
+++ Week2MovieProject/Program.cs
@@ -5,6 +5,14 @@ namespace Week2MovieProject
 {
     class Program
     {
+        public enum CrudOptions
+        {
+            CREATE,
+            READ,
+            UPDATE,
+            DELETE,
+            QUIT
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
@@ -12,34 +20,59 @@ namespace Week2MovieProject
             Console.WriteLine("1. Movies \n2. Quit");
             string input = Console.ReadLine();
 
-            switch (input.ToUpper())
+            switch (input)
             {
-                case "Movies":
-                    EnterMoviesSubMenu();
+                case "1":
+                    CrudMenu();
                     break;
-                case "Quit":
+                case "2":
                     // quit out of loop
                     break;
             }
 
         }
-
-        public static void EnterMoviesSubMenu()
+        public static void CrudMenu()
         {
             MovieController controller = new MovieController();
 
-            Console.WriteLine("1. Create \n2. Read");
-            string input = Console.ReadLine();
-
-            switch (input.ToUpper())
+            bool inMenu = true;
+            var values = Enum.GetValues(typeof(CrudOptions));
+            foreach (var value in values)
             {
-                case "Create":
-                    controller.CreateMovie();
-                    break;
-                case "Read":
-                    controller.ReadMovies();
-                    break;
-
+                Console.WriteLine(value);
             }
+            while (inMenu)
+            {
+                Console.Write("> ");
+                string input = Console.ReadLine();
+                // TryParse(stringInputToParse, ignoreCaseBoolean, outputVariable)
+                bool b = Enum.TryParse(input, true, out CrudOptions crudInput);
+                if (!b)
+                {
+                    Console.WriteLine("Invalid Input");
+                    continue;
+                }
+                switch (crudInput)
+                {
+                    case CrudOptions.CREATE:
+                        Console.WriteLine("Creating");
+                        controller.CreateMovie();
+                        break;
+                    case CrudOptions.READ:
+                        Console.WriteLine("Reading");
+                        controller.ReadMovies();
+                        break;
+                    case CrudOptions.DELETE:
+                        Console.WriteLine("Deleting");
+                      //  controller.ReadMovies();
+                        break;
+                    case CrudOptions.QUIT:
+                        inMenu = false;
+                        break;
+                }
+            }
+        }
+
     }
-}
+    }
+
